3ed0d68aa1f65b4ea749906309eaaf15ecd66164
Ivan Storck <ivanoats@gmail.com>
2014-02-24 07:24:18 -0800

    Adding inheritance

--- class.js
+++ class.js
@@ -7,10 +7,26 @@
 
 
 // A class "factory"
-var Class = function() {
+var Class = function(parent) {
   var klass = function() {
     this.init.apply(this, arguments);
   };
+
+  // Change klass' prototype
+  //
+  // If a parent is passed to the Class constructor, we make sure any
+  //  subclasses share the same prototype. This little dance around creating
+  //  a temporary anonymous function prevents instances from being created
+  //  when a class is inherited. The caveat here is that only instance
+  //  properties, not class properties, are inherited. There isn’t yet a
+  //  cross- browser way of setting an object’s [`__proto__;`](http://stackoverflow.com/questions/9959727/proto-vs-prototype-in-javascript). Libraries like
+  //  [Super.js](https://gist.github.com/mazesoul/957713) get around this problem by copying the properties,
+  //  rather than implementing proper dynamic inheritance.
+  if (parent) {
+    var Subclass = function(){};
+    Subclass.prototype = parent.prototype;
+    klass.prototype = new Subclass();
+  }
 
   klass.prototype.init = function(){};
 
@@ -19,6 +35,9 @@ var Class = function() {
 
   // Shortcut to access class
   klass.fn.parent = klass;
+
+  // Shortcut to access super class
+  klass._super = klass.__proto__;
 
   // Adding class properties
   klass.extend = function(obj) {
@@ -74,4 +93,19 @@ Person.extend({
   }
 });
 
+// Animal example
+
+var Animal = new Class();
+
+Animal.include({
+  breath: function(){
+    console.log('breath');
+  }
+});
+
+var Cat = new Class(Animal);
+
+// Animal Usage
+var tommy = new Cat();
+tommy.breath();
 
